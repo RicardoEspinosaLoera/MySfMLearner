b3b1f7c743fad6754b9c74b77791cd7b66cd1914
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-10-09 20:02:55 -0500

    training two

--- train_stage_two.py
+++ train_stage_two.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
-from trainer_stage_two import Trainer
+#from trainer_stage_two import Trainer
+from trainer_stage_two_new import Trainer
 from options import MonodepthOptions
 
 options = MonodepthOptions()
