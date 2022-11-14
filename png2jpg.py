8bcbb03a6f8d8140cbd32af592d57d4156f00c9c
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-11-14 12:20:50 -0600

    png2jpg

--- png2jpg.py
+++ png2jpg.py
@@ -5,18 +5,21 @@ import cv2
 
 path = "/workspace/dataset/"
 arr= ["Sequence_11-8-53-796","Sequence_12-18-52-88","Sequence_12-59-29-990"]
+id = 0
 for i in arr:    
     listd = os.listdir(path+i)
     for x in listd:
         lista = os.listdir(path+i+"/"+x)
+        lista.sort()
         for idx,y in enumerate(lista):
-            #name = y.split(".")[0]
-            #if "png" in y: 
-            # Load .png image
-            #image = cv2.imread(path+i+"/"+x+"/"+y)
-            # Save .jpg image
-            #cv2.imwrite(path+i+"/"+x+"/"+(idx+1)+'.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-            #os.remove(path+i+"/"+x+"/"+y)
-            print((idx+1))
-            os.rename(path+i+"/"+x+"/"+y,path+i+"/"+x+"/"+str(idx+1)+'.jpg')
+            name = y.split(".")[0]
+            if "png" in y: 
+                # Load .png image
+                image = cv2.imread(path+i+"/"+x+"/"+y)
+                # Save .jpg image
+                cv2.imwrite(path+i+"/"+x+"/"+(id+1)+'.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+                os.remove(path+i+"/"+x+"/"+y)
+                id = id+1
+                #print((idx+1))
+                #os.rename(path+i+"/"+x+"/"+y,path+i+"/"+x+"/"+str(idx+1)+'.jpg')
             
