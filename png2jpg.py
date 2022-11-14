8561799cfc2e080fbc43de3f0386dcfcb4aa7e56
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-11-14 11:25:49 -0600

    consecutive_frames

--- png2jpg.py
+++ png2jpg.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import cv2
 
-i = 0
+
 path = "/workspace/dataset/"
 arr= ["Sequence_11-8-53-796","Sequence_12-18-52-88","Sequence_12-59-29-990"]
 for i in arr:    
@@ -10,12 +10,12 @@ for i in arr:
     for x in listd:
         lista = os.listdir(path+i+"/"+x)
         for idx,y in enumerate(lista):
-            name = y.split(".")[0]
-            if "png" in y: 
-                # Load .png image
-                image = cv2.imread(path+i+"/"+x+"/"+y)
-                # Save .jpg image
-                cv2.imwrite(path+i+"/"+x+"/"+name+'.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-                os.remove(path+i+"/"+x+"/"+y)
-                print(y)
+            #name = y.split(".")[0]
+            #if "png" in y: 
+            # Load .png image
+            #image = cv2.imread(path+i+"/"+x+"/"+y)
+            # Save .jpg image
+            #cv2.imwrite(path+i+"/"+x+"/"+(idx+1)+'.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            #os.remove(path+i+"/"+x+"/"+y)
+            print((idx+1))
             
