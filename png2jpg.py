98f89a3276b0d66d822f551b52f526af6acb724b
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-11-14 17:01:01 -0600

    testing

--- png2jpg.py
+++ png2jpg.py
@@ -10,17 +10,12 @@ for i in arr:
     listd = os.listdir(path+i)
     listd.sort()
     for x in listd:
-        print(x)
         lista = os.listdir(path+i+"/"+x)
         for idx,y in enumerate(lista):
             name = y.split(".")[0]
-            print(y)
-            #if "png" in y: 
+            if "png" in y: 
                 # Load .png image
-                #image = cv2.imread(path+i+"/"+x+"/"+y)
+                image = cv2.imread(path+i+"/"+x+"/"+y)
                 # Save .jpg image
-                #cv2.imwrite(path+i+"/"+x+"/"+str(id+1)+'.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-                #os.remove(path+i+"/"+x+"/"+y)
-                #id = id+1
-                #print((idx+1))
-                #os.rename(path+i+"/"+x+"/"+y,path+i+"/"+x+"/"+str(idx+1)+'.jpg')
+                cv2.imwrite(path+i+"/"+x+"/"+str(idx+1)+'.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+                os.remove(path+i+"/"+x+"/"+y)
