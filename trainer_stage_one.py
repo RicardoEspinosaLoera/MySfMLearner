269aec9079da530fc2ff489a0b92b8297a7fbb66
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-11-21 22:41:08 -0600

    training

--- trainer_stage_one.py
+++ trainer_stage_one.py
@@ -301,6 +301,20 @@ class Trainer:
                        " | loss: {:.5f} | time elapsed: {} | time left: {}"
         print(print_string.format(self.epoch, batch_idx, samples_per_sec, loss,
                                   sec_to_hm_str(time_sofar), sec_to_hm_str(training_time_left)))
+    
+    def flow2rgb_raw(flow_map, max_value):
+        flow_map_np = flow_map.detach().cpu().numpy()
+        _, h, w = flow_map_np.shape
+        flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
+        rgb_map = np.ones((3,h,w)).astype(np.float32)
+        if max_value is not None:
+            normalized_flow_map = flow_map_np / max_value
+        else:
+            normalized_flow_map = flow_map_np / (np.abs(flow_map_np).max())
+        rgb_map[0] += normalized_flow_map[0]
+        rgb_map[1] -= 0.5*(normalized_flow_map[0] + normalized_flow_map[1])
+        rgb_map[2] += normalized_flow_map[1]
+        return rgb_map.clip(0,1)
 
     def log(self, mode, inputs, outputs, losses):
         """Write an event to the tensorboard events file
@@ -376,16 +390,3 @@ class Trainer:
         else:
             print("Cannot find Adam weights so Adam is randomly initialized")
 
-    def flow2rgb_raw(flow_map, max_value):
-        flow_map_np = flow_map.detach().cpu().numpy()
-        _, h, w = flow_map_np.shape
-        flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
-        rgb_map = np.ones((3,h,w)).astype(np.float32)
-        if max_value is not None:
-            normalized_flow_map = flow_map_np / max_value
-        else:
-            normalized_flow_map = flow_map_np / (np.abs(flow_map_np).max())
-        rgb_map[0] += normalized_flow_map[0]
-        rgb_map[1] -= 0.5*(normalized_flow_map[0] + normalized_flow_map[1])
-        rgb_map[2] += normalized_flow_map[1]
-        return rgb_map.clip(0,1)
