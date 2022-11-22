b3aac38523af0adf9fa507baa425b6774430c8c9
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-11-21 22:39:14 -0600

    triner_two

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -11,6 +11,10 @@ import torch.nn as nn
 from utils import *
 from layers import *
 from torch.utils.data import DataLoader
+import wandb
+
+wandb.init(project="AF-SfMLearner", entity="respinosa")
+
 from tensorboardX import SummaryWriter
 
 
@@ -679,3 +683,17 @@ class Trainer:
             # self.model_optimizer.load_state_dict(optimizer_dict)
         # else:
         print("Adam is randomly initialized")
+    
+    def flow2rgb_raw(flow_map, max_value):
+        flow_map_np = flow_map.detach().cpu().numpy()
+        _, h, w = flow_map_np.shape
+        flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
+        rgb_map = np.ones((3,h,w)).astype(np.float32)
+        if max_value is not None:
+            normalized_flow_map = flow_map_np / max_value
+        else:
+            normalized_flow_map = flow_map_np / (np.abs(flow_map_np).max())
+        rgb_map[0] += normalized_flow_map[0]
+        rgb_map[1] -= 0.5*(normalized_flow_map[0] + normalized_flow_map[1])
+        rgb_map[2] += normalized_flow_map[1]
+        return rgb_map.clip(0,1)
