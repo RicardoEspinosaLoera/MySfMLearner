ad6570357d57bbf12169b7ed0b984e729b42c0db
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-08 11:31:55 +0200

    height

--- options.py
+++ options.py
@@ -46,7 +46,7 @@ class MonodepthOptions:
         self.parser.add_argument("--height",
                                  type=int,
                                  help="input image height",
-                                 default=216)
+                                 default=224)
         self.parser.add_argument("--width",
                                  type=int,
                                  help="input image width",
