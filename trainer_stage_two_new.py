5183f38f2c3bd63df891551d5353284a02725d7c
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-14 19:23:48 +0200

    wandb

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -624,9 +624,9 @@ class Trainer:
                     
                     wandb.log({mode+"_refinedCB_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs["refinedCB_"+str(frame_id)+"_"+str(s)][j].data)},step=self.step)
 
-                    #wandb.log({mode+"_Brightness_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs["b_"+str(s)+"_"+str(frame_id)][j].data)},step=self.step)
+                    wandb.log({mode+"_Brightness_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs["bh_"+str(s)+"_"+str(frame_id)][j].data)},step=self.step)
 
-                    #wandb.log({mode+"_Constrast_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs["c_"+str(s)+"_"+str(frame_id)][j].data)},step=self.step)
+                    wandb.log({mode+"_Constrast_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs["ch_"+str(s)+"_"+str(frame_id)][j].data)},step=self.step)
                     
  
                     if s == 0:
