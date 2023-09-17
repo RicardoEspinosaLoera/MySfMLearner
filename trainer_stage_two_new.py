363f24ac356bcd44fc391d2b96f84ab87ff3aaa4
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-17 20:03:47 +0200

    log

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -638,7 +638,7 @@ class Trainer:
                 #writer.add_image(
                 #    "disp_{}/{}".format(s, j),
                 #   normalize_image(outputs[("disp", s)][j]), self.step)
-                wandb.log({mode+"_disp_{}/{}".format(s, j): wandb.Image(normalize_image(outputs["disp_"+str(s)][j]))},step=self.step)
+                wandb.log({mode+"_disp_{}_{}".format(s, j): wandb.Image(normalize_image(outputs["disp_"+str(s)][j]))},step=self.step)
                 #wandb.log({mode+"_refinedCB_{}_{}".format(frame_id, s): wandb.Image(outputs["refinedCB_"+str(frame_id)+"_"+str(s)].data)},step=self.step)
                 
                     
