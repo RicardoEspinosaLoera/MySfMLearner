89591e6fd1aaf96db0c02ad0498efee891f4bf20
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-19 11:56:47 +0200

    transform

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -493,9 +493,11 @@ class Trainer:
                 #print(outputs["color_"+str(frame_id)+"_"+str(scale)].shape)
                 #print(inputs[("color",0,)].shape)
                 loss_reprojection += (
+                    #self.compute_reprojection_loss(outputs[("color", frame_id, scale)], outputs[("refined", scale, frame_id)]) * occu_mask_backward).sum() / occu_mask_backward.sum()
                     self.compute_reprojection_loss(outputs["color_"+str(frame_id)+"_"+str(scale)], inputs[("color",0,0)]) * occu_mask_backward).sum() / occu_mask_backward.sum()
                 loss_transform += (
-                    torch.abs(outputs["refinedCB_"+str(frame_id)+"_"+str(scale)] - outputs["r_"+str(scale)+"_"+str(frame_id)].detach()).mean(1, True) * occu_mask_backward).sum() / occu_mask_backward.sum()
+                    #torch.abs(outputs[("refined", scale, frame_id)] - outputs[("registration", 0, frame_id)].detach()).mean(1, True) * occu_mask_backward).sum() / occu_mask_backward.sum()
+                    torch.abs(outputs["color_"+str(frame_id)+"_"+str(scale)] - inputs[("color",0,0)]).mean(1, True) * occu_mask_backward).sum() / occu_mask_backward.sum()
                     # self.compute_reprojection_loss(outputs[("refined", scale, frame_id)], outputs[("registration", 0, frame_id)].detach()) * occu_mask_backward).sum() / occu_mask_backward.sum()
                 #loss_cvt += get_smooth_bright(outputs["bh_"+str(scale)+"_"+str(frame_id)], inputs[("color", 0, 0)], outputs["r_"+str(scale)+"_"+str(frame_id)].detach(), occu_mask_backward)
 
