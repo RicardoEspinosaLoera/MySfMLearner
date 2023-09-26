a0079974fb8a532c91c1a4934df11b8b579affd1
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-26 11:41:41 +0200

    predcition depth

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -439,9 +439,9 @@ class Trainer:
                     #wandb.log({"BH_{}_{}".format(frame_id, scale): wandb.Image(outputs["bh_"+str(scale)+"_"+str(frame_id)].data)},step=self.step)
                     #wandb.log({"refinedCB_{}_{}".format(frame_id, scale): wandb.Image(outputs["refinedCB_"+str(frame_id)+"_"+str(scale)].data)},step=self.step)
         #Feature similairty 
-        #self.models["encoder"].eval()
-        #outputs["f2"] = self.models["encoder"](outputs["color_"+str(-1)+"_"+str(0)])[0][:,r,:, :]
-        #self.models["encoder"].train()    
+        self.models["encoder"].eval()
+        outputs["f2"] = self.models["encoder"](outputs["color_"+str(-1)+"_"+str(0)])[0][:,r,:, :]
+        self.models["encoder"].train()    
                 
     def compute_reprojection_loss(self, pred, target):
 
