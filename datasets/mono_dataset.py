1bba78c9d1f0e91c52931988e086edc7af99a0b8
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-27 09:28:34 +0200

    aug

--- datasets/mono_dataset.py
+++ datasets/mono_dataset.py
@@ -134,9 +134,7 @@ class MonoDataset(data.Dataset):
         inputs = {}
 
         do_color_aug = self.is_train and random.random() > 0.5
-        #do_color_aug = False
-        #do_flip = False
-        self.is_train and random.random() > 0.5
+        do_flip = self.is_train and random.random() > 0.5
 
         line = self.filenames[index].split()
         folder = line[0]
