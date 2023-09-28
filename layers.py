aa5a2241634cbee96634d3a1ac066fd57b7144a1
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-28 16:42:01 +0200

    img_gray = transforms.Grayscale()

--- layers.py
+++ layers.py
@@ -251,7 +251,7 @@ def get_feature_similarity_loss(source,warped):
     return ldepth
 
 def get_ilumination_invariant_features(img):
-    img_gray = transform = transforms.Grayscale()
+    img_gray = transforms.Grayscale()
 
     print(img_gray.shape)
 
