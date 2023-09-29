e8731c2868962d9608c44467ffa2cb2174cf6523
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-29 22:22:18 +0200

    print

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -483,7 +483,7 @@ class Trainer:
 
         ssim_loss = self.ssim(features_p, features_t).mean(1, True)
         ii_loss = 0.85 * ssim_loss + 0.15 * l1_loss
-        print(ii_loss.shape)
+        #print(ii_loss.shape)
         return ii_loss
     
 
