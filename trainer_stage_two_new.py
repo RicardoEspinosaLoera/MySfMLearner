896f8e661aa61bd4d131406cdc00dddf03a3d998
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-08 14:48:42 +0200

    shape

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -353,7 +353,7 @@ class Trainer:
 
                     # Input motion flow
                     outputs_mf = self.models["motion_flow"](pose_inputs[0])
-
+                    print(outputs_mf.shape)
                     outputs["axisangle_0_"+str(f_i)] = axisangle
                     outputs["translation_0_"+str(f_i)] = translation
                     outputs["cam_T_cam_0_"+str(f_i)] = transformation_from_parameters(
@@ -417,7 +417,7 @@ class Trainer:
                 outputs["sample_"+str(frame_id)+"_"+str(scale)] = pix_coords
                 #print(outputs["sample_"+str(frame_id)+"_"+str(scale)].shape)
                 #outputs["mf_"+str(scale)] = outputs["mf_"+str(scale)].reshape(12,256,128,2)
-                outputs["sample_"+str(frame_id)+"_"+str(scale)] = outputs["sample_"+str(frame_id)+"_"+str(scale)] + outputs["mf_"+str(scale)].view(12, 256, 128, 2)
+                outputs["sample_"+str(frame_id)+"_"+str(scale)] = outputs["sample_"+str(frame_id)+"_"+str(scale)] + outputs["mf_"+str(scale)]
 
                 outputs["color_"+str(frame_id)+"_"+str(scale)] = F.grid_sample(
                     inputs[("color", frame_id, source_scale)],
