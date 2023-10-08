2b70eb8a44ea89cbff2b5e8db2128030f993b734
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-08 20:24:48 +0200

    flow print

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -698,6 +698,20 @@ class Trainer:
                 wandb.log({mode+"_disp_{}_{}".format(s, j): wandb.Image(normalize_image(outputs["disp_"+str(s)][j]))},step=self.step)
                                 
                     
+    def flow2rgb_raw(selg,flow_map, max_value):
+        flow_map_np = flow_map.detach().cpu().numpy()
+        _, h, w = flow_map_np.shape
+        flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
+        rgb_map = np.ones((3,h,w)).astype(np.float32)
+        if max_value is not None:
+            normalized_flow_map = flow_map_np / max_value
+        else:
+            normalized_flow_map = flow_map_np / (np.abs(flow_map_np).max())
+        rgb_map[0] += normalized_flow_map[0]
+        rgb_map[1] -= 0.5*(normalized_flow_map[0] + normalized_flow_map[1])
+        rgb_map[2] += normalized_flow_map[1]
+        return rgb_map.clip(0,1)
+
 
     def save_opts(self):
         """Save options to disk so we know what we ran this experiment with
