b3de8297bd0251b078933788ddde623600107792
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-08 15:04:17 +0200

    interpolate

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -365,7 +365,9 @@ class Trainer:
                     for scale in self.opt.scales:
                         outputs["b_"+str(scale)+"_"+str(f_i)] = outputs_lighting[("lighting", scale)][:,0,None,:, :]
                         outputs["c_"+str(scale)+"_"+str(f_i)] = outputs_lighting[("lighting", scale)][:,1,None,:, :]
-                        outputs["mf_"+str(scale)] = outputs_mf[("flow", scale)].view(12,256,128,2)
+                        outputs["mf_"+str(scale)] =  disp = F.interpolate(
+                    outputs_mf[("flow", scale)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
+
                     
                    
                     
