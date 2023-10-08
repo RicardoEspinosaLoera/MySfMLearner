1c9d7456a0b35a5d6762b125cc15c6b35a5a5184
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-08 15:17:21 +0200

    flow

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -416,13 +416,13 @@ class Trainer:
                     cam_points, inputs[("K", source_scale)], T)
 
                 outputs["sample_"+str(frame_id)+"_"+str(scale)] = pix_coords
-                print(outputs["sample_"+str(frame_id)+"_"+str(scale)].shape)
-                print(outputs["mf_"+str(scale)].shape)
-                #outputs["mf_"+str(scale)] = outputs["mf_"+str(scale)].reshape(12,256,128,2)
-                #flow = F.interpolate(
-                #    outputs["mf_"+str(scale)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
-                #outputs["sample_"+str(frame_id)+"_"+str(scale)] = outputs["sample_"+str(frame_id)+"_"+str(scale)] + flow
 
+                #outputs["mf_"+str(scale)] = outputs["mf_"+str(scale)].reshape(12,256,128,2)
+                flow = F.interpolate(
+                    outputs["mf_"+str(scale)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
+                #outputs["sample_"+str(frame_id)+"_"+str(scale)] = outputs["sample_"+str(frame_id)+"_"+str(scale)] + flow
+                print(outputs["sample_"+str(frame_id)+"_"+str(scale)].shape)
+                print(flow.shape)
                 outputs["color_"+str(frame_id)+"_"+str(scale)] = F.grid_sample(
                     inputs[("color", frame_id, source_scale)],
                     outputs["sample_"+str(frame_id)+"_"+str(scale)],
