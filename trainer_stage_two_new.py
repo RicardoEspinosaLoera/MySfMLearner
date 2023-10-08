9c9fc6bb49151f12559138c2bfd08cff3a6bf070
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-08 20:29:37 +0200

    flow2rgb_raw

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -671,6 +671,20 @@ class Trainer:
             " | loss: {:.5f} | time elapsed: {} | time left: {}"
         print(print_string.format(self.epoch, batch_idx, samples_per_sec, loss,
                                   sec_to_hm_str(time_sofar), sec_to_hm_str(training_time_left)))
+                                  
+    def flow2rgb_raw(self,flow_map, max_value):
+        flow_map_np = flow_map.detach().cpu().numpy()
+        _, h, w = flow_map_np.shape
+        flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
+        rgb_map = np.ones((3,h,w)).astype(np.float32)
+        if max_value is not None:
+            normalized_flow_map = flow_map_np / max_value
+        else:
+            normalized_flow_map = flow_map_np / (np.abs(flow_map_np).max())
+        rgb_map[0] += normalized_flow_map[0]
+        rgb_map[1] -= 0.5*(normalized_flow_map[0] + normalized_flow_map[1])
+        rgb_map[2] += normalized_flow_map[1]
+        return rgb_map.clip(0,1)
 
     def log(self, mode, inputs, outputs, losses):
         """Write an event to the tensorboard events file
@@ -693,25 +707,13 @@ class Trainer:
                 f = outputs["mf_"+str(s)][j].data
                 flow = self.flow2rgb_raw(f,32)
                 flow = torch.from_numpy(flow)
-                wandb.log({mode+"_Motion_Flow_{}_{}".format(s, j): wandb.Image(normalize_image(flow))},step=self.step)
+                wandb.log({mode+"_Motion_Flow_{}_{}".format(s, j): wandb.Image(flow)},step=self.step)
                     
  
                 wandb.log({mode+"_disp_{}_{}".format(s, j): wandb.Image(normalize_image(outputs["disp_"+str(s)][j]))},step=self.step)
                                 
                     
-    def flow2rgb_raw(self,flow_map, max_value):
-        flow_map_np = flow_map.detach().cpu().numpy()
-        _, h, w = flow_map_np.shape
-        flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
-        rgb_map = np.ones((3,h,w)).astype(np.float32)
-        if max_value is not None:
-            normalized_flow_map = flow_map_np / max_value
-        else:
-            normalized_flow_map = flow_map_np / (np.abs(flow_map_np).max())
-        rgb_map[0] += normalized_flow_map[0]
-        rgb_map[1] -= 0.5*(normalized_flow_map[0] + normalized_flow_map[1])
-        rgb_map[2] += normalized_flow_map[1]
-        return rgb_map.clip(0,1)
+
 
 
     def save_opts(self):
