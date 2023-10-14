7df43e16b7b235e501e4ac9d6f27b51b53d37560
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-14 17:24:35 +0200

    list zip

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -366,7 +366,7 @@ class Trainer:
 
                     #print(len(motion_inputs))
                     #print(len(pose_inputs))
-                    axisangle, translation = self.models["pose"](torch.cat(pose_inputs,motion_inputs))
+                    axisangle, translation = self.models["pose"](list(zip(pose_inputs, motion_inputs)))
                     #axisangle, translation = self.models["pose_ii"](pose_inputs)
 
                     # Input for Lighting
