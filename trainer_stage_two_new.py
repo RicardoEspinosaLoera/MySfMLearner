cc6a4078c19e6822b8371224c1224c8a9ea2a7d2
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-19 10:51:41 +0200

    pose iif

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -360,19 +360,19 @@ class Trainer:
                     # Input motion flow
                     
                     pose_inputs = [self.models["pose_encoder"](torch.cat(inputs_all, 1))]
-                    """a = F.interpolate(
+                    a = F.interpolate(
                             pose_feats[f_i], [self.opt.height + 2, self.opt.width + 2], mode="bilinear", align_corners=True)
 
                     b = F.interpolate(
                             pose_feats[0], [self.opt.height + 2, self.opt.width + 2], mode="bilinear", align_corners=True)
 
                     iif_all = [get_ilumination_invariant_features(a),get_ilumination_invariant_features(b)] 
-                    """
+                    
                     #motion_inputs = [self.models["encoder"](torch.cat(pose_inputs, 1))]
                     #outputs_mf = self.models["motion_flow"](motion_inputs[0])
-                    #input_combined = pose_inputs
-                    #input_combined[:][:] = zip(pose_inputs[:][:], motion_inputs[:][:])
-                    axisangle, translation = self.models["pose"](pose_inputs)
+                    input_combined = pose_inputs
+                    input_combined[:][:] = zip(pose_inputs[:][:], motion_inputs[:][:])
+                    axisangle, translation = self.models["pose_ii"](input_combined)
                     #axisangle, translation = self.models["pose_ii"](pose_inputs)
 
                     # Input for Lighting
