15100ffa9e9f57622fff2885a8fff4390a6c4d34
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-19 23:33:39 +0200

    boolean

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -721,7 +721,7 @@ class Trainer:
                     flow = torch.from_numpy(flow)
                     wandb.log({mode+"_Motion_Flow_{}_{}_{}".format(frame_id,s,j): wandb.Image(flow)},step=self.step)
                 
-                disp = self.colormap(outputs["disp_"+str(s)][j])
+                disp = self.colormap(outputs["disp_"+str(s)][j, 0])
                 wandb.log({mode+"_Disp_{}_{}".format(s, j): wandb.Image(disp.transpose(1, 2, 0))},step=self.step)
                                 
                     
