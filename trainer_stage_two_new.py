e1f41e6d176f7b6c5986c9ad1d36236afe82c369
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-20 14:40:27 +0200

    gc

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch.optim as optim
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import gc
 # generate random integer values
  
 _DEPTH_COLORMAP = plt.get_cmap('plasma', 256)  # for plotting
