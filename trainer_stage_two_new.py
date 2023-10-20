b73c5897a8f6eee54c0ab36677fd76cb5dfb3716
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-20 17:33:26 +0200

    typo

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -579,7 +579,7 @@ class Trainer:
                 loss_reprojection += (
                     self.compute_reprojection_loss(outputs["refinedCB_"+str(frame_id)+"_"+str(scale)], inputs[("color",0,0)]) * occu_mask_backward).sum() / occu_mask_backward.sum()"""
                 loss_reprojection += (
-                    self.compute_reprojection_loss(outputs[("color", frame_id, scale)], outputs[("refinedCB_", frame_id, scale)]) * occu_mask_backward).sum() / occu_mask_backward.sum()
+                    self.compute_reprojection_loss( outputs["color_"+str(frame_id)+"_"+str(scale)], outputs[("refinedCB_", frame_id, scale)]) * occu_mask_backward).sum() / occu_mask_backward.sum()
                 """loss_ilumination_invariant += (
                     self.get_ilumination_invariant_loss(outputs["color_"+str(frame_id)+"_"+str(scale)], inputs[("color",0,0)]) * occu_mask_backward_).sum() / occu_mask_backward_.sum()
                 loss_motion_flow += (
