b06e2dc29297181ed90874ff9fb3d7c1119dbd2b
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-20 16:29:35 +0200

    no motion flow

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -365,24 +365,20 @@ class Trainer:
 
                     # Input motion flow
                     pose_inputs = [self.models["pose_encoder"](torch.cat(inputs_all, 1))]
-                    """
+                    
                     iif_all = [get_ilumination_invariant_features(pose_feats[f_i]),get_ilumination_invariant_features( pose_feats[0])] 
                     
                     motion_inputs = [self.models["ii_encoder"](torch.cat(iif_all, 1))]
                     outputs_mf = self.models["motion_flow"](motion_inputs[0])
                     input_combined = pose_inputs
-                    #print(pose_inputs[0][0].shape)
-                    #print(motion_inputs[0][0].shape)
-                    #input_combined[:][:] = zip(pose_inputs[:][:], motion_inputs[:][:])
-                    #concatenated_output = torch.cat((pose_inputs,motion_inputs), dim=1)
                     concatenated_list = []
                     # Iterate over the corresponding tensors in list1 and list2 and concatenate them
                     for tensor1, tensor2 in zip(pose_inputs[0], motion_inputs[0]):
                         concatenated_tensor = torch.cat([tensor1, tensor2], dim=1)
                         concatenated_list.append(concatenated_tensor)
                     
-                    axisangle, translation = self.models["pose"]([concatenated_list])"""
-                    axisangle, translation = self.models["pose"](pose_inputs)
+                    axisangle, translation = self.models["pose"]([concatenated_list])
+                    #axisangle, translation = self.models["pose"](pose_inputs)
 
                     # Input for Lighting
                     outputs_lighting = self.models["lighting"](pose_inputs[0])                   
