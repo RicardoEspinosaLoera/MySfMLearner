b6f011555c4953b3ab33c6144ac024515b2dc105
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-22 02:28:00 +0200

    loss_motion_flow

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -556,9 +556,9 @@ class Trainer:
                     self.compute_reprojection_loss( outputs["color_"+str(frame_id)+"_"+str(scale)], outputs["refinedMF_"+str(frame_id)+"_"+str(scale)]) * occu_mask_backward).sum() / occu_mask_backward.sum()
                 """loss_ilumination_invariant += (
                     self.get_ilumination_invariant_loss(outputs["color_"+str(frame_id)+"_"+str(scale)], inputs[("color",0,0)]) * occu_mask_backward_).sum() / occu_mask_backward_.sum()"""
-                """loss_motion_flow += (
+                loss_motion_flow += (
                     self.get_motion_flow_loss(outputs["mf_"+str(scale)+"_"+str(frame_id)])
-                )"""
+                )
             
 
             mean_disp = disp.mean(2, True).mean(3, True)
