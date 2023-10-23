560e5b3b34d2f3d498c872a6b2ccda9f53a97a3a
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-23 14:56:01 +0200

    f_i

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -405,7 +405,7 @@ class Trainer:
                     b = outputs["b_"+str(0)+"_"+str(f_i)]
                     c = outputs["c_"+str(0)+"_"+str(f_i)]
                     outputs["refinedCB_"+str(f_i)+"_"+str(scale)] = c * outputs["color_motion_"+str(f_i)+"_"+str(scale)] + b
-                    
+                    print(f_i)
                    
                     
         return outputs
