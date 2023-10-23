d5d004b69740a3b8d8dff0f929c601df4c697fe8
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-23 13:27:43 +0200

    loss_motion_flow

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -542,7 +542,7 @@ class Trainer:
         # The coefficients are designed in a way that the norm asymptotes to L1 in
         # the small value limit.
         #return torch.mean(2 * mean * torch.sqrt(tensor_abs / (mean + 1e-24) + 1))
-        return mean * torch.sqrt(tensor_abs / (mean + 1e-24) + 1)
+        return torch.mean(mean * torch.sqrt(tensor_abs / (mean + 1e-24) + 1))
         #return torch.sqrt(tensor_abs / mean + 1))
 
     
