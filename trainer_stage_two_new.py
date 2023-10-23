e8fa4081d5005c4df4e7e7d12ffed6fa9fc92c44
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-23 17:45:37 +0200

    _Output_

--- trainer_stage_two_new.py
+++ trainer_stage_two_new.py
@@ -724,17 +724,18 @@ class Trainer:
                 for frame_id in self.opt.frame_ids[1:]:
                     #if frame_id < 0:
                     wandb.log({mode+"_Output_{}_{}_{}".format(frame_id, s, j): wandb.Image(outputs["color_"+str(frame_id)+"_"+str(s)][j].data)},step=self.step)
-                    wandb.log({mode+"_Output_Motion_{}_{}_{}".format(frame_id, s, j): wandb.Image(outputs["color_motion_"+str(frame_id)+"_"+str(s)][j].data)},step=self.step)
+                    #wandb.log({mode+"_Output_Motion_{}_{}_{}".format(frame_id, s, j): wandb.Image(outputs["color_motion_"+str(frame_id)+"_"+str(s)][j].data)},step=self.step)
                     wandb.log({mode+"_Refined_{}_{}_{}".format(frame_id, s, j): wandb.Image(outputs["refinedCB_"+str(frame_id)+"_"+str(s)][j].data)},step=self.step)
                     """
                     wandb.log({mode+"_Brightness_{}_{}_{}".format(frame_id, s, j): wandb.Image(outputs["b_"+str(s)+"_"+str(frame_id)][j].data)},step=self.step)
 
                     wandb.log({mode+"_Contrast_{}_{}_{}".format(frame_id, s, j): wandb.Image(outputs["c_"+str(s)+"_"+str(frame_id)][j].data)},step=self.step)
                     """
+                """
                 f = outputs["mf_"+str(s)+"_"+str(frame_id)][j].data
                 flow = self.flow2rgb(f,32)
                 flow = torch.from_numpy(flow)
-                wandb.log({mode+"_Motion_Flow_{}_{}".format(s,j): wandb.Image(flow)},step=self.step)
+                wandb.log({mode+"_Motion_Flow_{}_{}".format(s,j): wandb.Image(flow)},step=self.step)"""
                     
                 disp = normalize_image(outputs["disp_"+str(s)][j])
                 wandb.log({mode+"_Disp_{}_{}".format(s, j): wandb.Image(disp)},step=self.step)
